71ccfd445ac383dc3a2efdafdcb22c81c1b34307
josuehoenicka <josuee.vzla@gmail.com>
2025-03-13 12:05:01 -0300

    update: middleware, controller, route added

--- index.js
+++ index.js
@@ -1,75 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model.ts");
+const productRoutes = require("./routes/product.route.js");
 const app = express();
 
-app.use(express.json());
-
+// MongoDB Data
 const userDB = process.env.USER; // Estos valores vienen del fichero ".env" que por defecto oculta git (acá debes poner tus datos de Closter de MongoDB)
 const passwordDB = process.env.PASSWORD; // Estos valores vienen del fichero ".env" que por defecto oculta git (acá debes poner tus datos de Closter de MongoDB)
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // Activa el form url encoded
+
+// Routes
+app.use("/api/products", productRoutes);
+
+// Run Server
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
-});
-
-// GETS
-app.get("/", (req, res) => {
-  res.send("Hello from Node API Test");
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// POSTS
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// PUTS
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) return res.status(404).json({ message: "Product not found" });
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// DELETES
-app.delete("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) return res.status(404).json({ message: "Product not found" });
-    res.status(200).json({ message: `${product.name} deleted successfully` });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 mongoose
